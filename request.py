8bf22b1648f4d9310662383617202f4ccb6704b4
ulyasha <argentia.bloom@gmail.com>
2023-02-24 13:59:02 +0300

    without request.py

--- request.py
+++
@@ -1,9 +0,0 @@
-import requests
-import config
-
-def get_city_coord(city):
-    payload = {'geocode': city, 'apikey': config.geo_key, 'format': 'json'}
-    r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
-    return r
-
-print(get_city_coord('Москва'))
